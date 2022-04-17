f7f20b72df2682123e2698d10ad92d459705990a
ireneha <43556513+ireneha@users.noreply.github.com>
2022-04-16 21:46:42 -0500

    insert values but not completed

---
+++ insertFile.sql
@@ -0,0 +1,67 @@
+delete from manager;
+delete from employee;
+delete from product;
+delete from customer;
+delete from supplier;
+delete from payment_method;
+delete from promotion;
+delete from membership;
+delete from item;
+delete from department;
+delete from shelf;
+delete from paycheck;
+delete from store;
+delete from warehouse;
+delete from dependents;
+delete from shipment;
+delete from delivery;
+delete from help_ticket;
+delete from invoice;
+delete from orders;
+
+insert into manager values ('m91', 'David', 'Walter', '90000', '012342323');
+insert into manager values ('m203', 'Anna', 'Bailey', '130000', '543890021');
+insert into manager values ('m033', 'Morris', 'Rogers', '81600', '238504184');
+insert into manager values ('m68', 'Jake', 'Peterson', '88300', '836501793');
+insert into manager values ('m84', 'Kayla', 'Ruiz', '91700', '153896204');
+insert into manager values ('m012', 'Andrew', 'Powell', '82000', '131452453');
+insert into manager values ('m220', 'Tomas', 'Perry', '1280000', '223116486');
+insert into manager values ('m153', 'Harry', 'Walter', '97500', '094153152');
+insert into manager values ('m118', 'David', 'Ruiz', '94200', '665473212');
+
+insert into employee values ('e111','Liam', 'Robinson', '44000','357243214');
+insert into employee values ('e134','Young', 'Lee', '58300','333241374');
+insert into employee values ('e180','Noah', 'Scott', '48000','116343271');
+insert into employee values ('e145','James', 'Peterson', '52700','908087341');
+insert into employee values ('e177','Olivia', 'Torres', '51500','212106477');
+insert into employee values ('e129','Henry', 'Wright', '29000','715232412');
+insert into employee values ('e116','Evelyn', 'Hill', '56000','283536442');
+insert into employee values ('e104','Sophie', 'Rivera', '43000','038385436');
+insert into employee values ('e162','Lucas', 'Campell', '18300','117664354');
+
+insert into product values('be01','Beverages');
+insert into product values('ca01','Canned');
+insert into product values('da22','Dairy');
+insert into product values('ff01','Frozen food');
+insert into product values('ba03','Bakery');
+insert into product values('me02','Meat');
+insert into product values('cl90','Cleaners');
+insert into product values('pc01','Personal care');
+insert into product values('da24','Dairy');
+insert into product values('ca02','Canned');
+insert into product values('ff02','Frozen food');
+insert into product values('me01','Meat');
+insert into product values('me03','Meat');
+insert into product values('da26','Dairy');
+insert into product values('be02','Beverages');
+insert into product values('be03','Beverages');
+insert into product values('ba01','Bakery');
+insert into product values('be04','Baverages');
+insert into product values('ff03','Frozen food');
+insert into product values('pc02','Personal care');
+insert into product values('ca03','Canned');
+insert into product values('cl91','Cleaners');
+insert into product values('ba02','Bakery');
+
+
+
